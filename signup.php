e560a7bfdc758dbbf6cb01018922de0bfbbd897c
neyomy1 <104121392+neyomy1@users.noreply.github.com>
2023-10-18 01:55:28 -0400

    added a back to login option if user logs out and fixed a minor error

--- signup.php
+++ signup.php
@@ -1,25 +1,32 @@
 <?php
+// Error and session handling setup
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 session_start();
 
-// Database connection file
-include 'config.php';
+// Redirect if user is already logged in
+if (isset($_SESSION['username'])) {
+    header('Location: index.php');
+    exit;
+}
+
+// Database connection
+require 'config.php';
 
 $error = "";
 
+// Signup logic
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username'];
-    $password1 = $_POST['password1'];
-    $password2 = $_POST['password2'];
+    $username = trim($_POST['username']);
+    $password1 = trim($_POST['password1']);
+    $password2 = trim($_POST['password2']);
 
-    // Check if passwords match
-    if ($password1 != $password2) {
+    if ($password1 !== $password2) {
         $error = "Passwords do not match!";
     } else {
-        // Check if username already exists
+        // Check for existing username
         $stmt = $conn->prepare("SELECT username FROM users WHERE username = ?");
         $stmt->bind_param("s", $username);
         $stmt->execute();
@@ -31,36 +38,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $hashedPassword = password_hash($password1, PASSWORD_DEFAULT);
             $stmt = $conn->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
             $stmt->bind_param("ss", $username, $hashedPassword);
+            
             if ($stmt->execute()) {
-                // Logs in the user after signup
                 $_SESSION['username'] = $username;
-
-                // Takes the user to index.html if theysignup successfully
-                header('Location: index.html');
+                header('Location: index.php');
                 exit;
             } else {
-                $error = "There was a problem with the signup. Please try again.";
+                $error = "Error during signup. Please try again.";
             }
         }
     }
 }
 ?>
 
-<!--HTML code -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Signup</title>
+    <title>Signup - FaveTune</title>
 </head>
 <body>
 <h2>FaveTune SignUp</h2>
-<!--SignUp form-->
+
 <form action="signup.php" method="post">
     <div>
-        <p>Please fill this form to create an account</p>
+        <p>Please fill this form to create an account:</p>
         <label for="username">Username:</label>
         <input type="text" name="username" id="username" required>
     </div>
@@ -76,14 +80,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <input type="submit" value="Signup">
         <input type="reset" value="reset">
     </div>
-    <p>Already have an account? <a href="Login.php">Login here</a> </p>
+    <p>Already have an account? <a href="Login.php">Login here</a></p>
 </form>
 
-<?php
-if ($error) {
-    echo "<p style='color:red;'>$error</p>";
-}
-?>
+<?php if ($error): ?>
+    <p style='color:red;'><?= htmlspecialchars($error) ?></p>
+<?php endif; ?>
 
 </body>
 </html>
