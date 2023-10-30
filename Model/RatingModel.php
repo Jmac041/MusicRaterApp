8b959019265efc292c5cc43cdfbe334ad1ba24bd
Jmac041 <144952777+Jmac041@users.noreply.github.com>
2023-10-30 10:39:53 -0400

    Update RatingModel.php

--- Model/RatingModel.php
+++ Model/RatingModel.php
@@ -1,47 +1,183 @@
 <?php
-
-class RatingModel extends Database
+class RatingController extends BaseController
 {
-    public function getRatings()
+    public function listAction()
     {
-        // Need to implement the logic to retrieve all ratings
-        $sql = "SELECT * FROM ratings_table";
-        $ratings = $this->select($sql);
-        return $ratings;
-    }
-
-    public function getRating($ratingId)
-    {
-        // Need to implement the logic to retrieve a specific rating by its ID
-        $sql = "SELECT * FROM ratings_table WHERE id = ?";
-        $rating = $this->select($sql, ["i", $ratingId]);
-
-        if (count($rating) > 0) {
-            return $rating[0];
+        $strErrorDesc = '';
+        $requestMethod = $_SERVER["REQUEST_METHOD"];
+        
+        if (strtoupper($requestMethod) == 'GET') {
+            try {
+                // Instantiate a RatingModel and call the getRatings method to fetch all ratings
+                $ratingModel = new RatingModel();
+                $ratings = $ratingModel->getRatings();
+                $responseData = json_encode($ratings);
+            } catch (Exception $e) {
+                $strErrorDesc = $e->getMessage() . ' Something went wrong! Please contact support.';
+                $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+            }
+        } else {
+            $strErrorDesc = 'Method not supported';
+            $strErrorHeader = 'HTTP/1.1 422 Unprocessable Entity';
         }
 
-        return null;
+        if (!$strErrorDesc) {
+            $this->sendOutput(
+                $responseData,
+                array('Content-Type: application/json', 'HTTP/1.1 200 OK')
+            );
+        } else {
+            $this->sendOutput(json_encode(array('error' => $strErrorDesc)),
+                array('Content-Type: application/json', $strErrorHeader)
+            );
+        }
+    }
+    
+    public function viewAction()
+    {
+        $strErrorDesc = '';
+        $requestMethod = $_SERVER["REQUEST_METHOD"];
+
+        if (strtoupper($requestMethod) == 'GET') {
+            $ratingId = $_GET['id']; // Assuming the rating ID is passed as a query parameter
+            if (empty($ratingId) || !is_numeric($ratingId)) {
+                $strErrorDesc = 'Invalid rating ID';
+                $strErrorHeader = 'HTTP/1.1 400 Bad Request';
+            } else {
+                try {
+                    // Instantiate a RatingModel and call the getRating method to fetch a specific rating
+                    $ratingModel = new RatingModel();
+                    $rating = $ratingModel->getRating($ratingId);
+                    if ($rating) {
+                        $responseData = json_encode($rating);
+                    } else {
+                        $strErrorDesc = 'Rating not found';
+                        $strErrorHeader = 'HTTP/1.1 404 Not Found';
+                    }
+                } catch (Exception $e) {
+                    $strErrorDesc = $e->getMessage() . ' Something went wrong! Please contact support.';
+                    $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+                }
+            }
+        } else {
+            $strErrorDesc = 'Method not supported';
+            $strErrorHeader = 'HTTP/1.1 422 Unprocessable Entity';
+        }
+
+        if (!$strErrorDesc) {
+            $this->sendOutput(
+                $responseData,
+                array('Content-Type: application/json', 'HTTP/1.1 200 OK')
+            );
+        } else {
+            $this->sendOutput(json_encode(array('error' => $strErrorDesc)),
+                array('Content-Type: application/json', $strErrorHeader)
+            );
+        }
     }
 
-    public function createRating($username, $song, $rating)
+    public function createAction()
     {
-        // Need to implement the logic to create a new rating
-        $sql = "INSERT INTO ratings_table (username, song, rating) VALUES (?, ?, ?)";
-        $this->insert($sql, ["sss", $username, $song, $rating]);
+        $strErrorDesc = '';
+        $requestMethod = $_SERVER["REQUEST_METHOD"];
+        if (strtoupper($requestMethod) == 'POST') {
+            $postData = json_decode(file_get_contents('php://input'), true);
+
+            try {
+                // You will need to implement the logic to create a new rating here
+                // Instantiate a RatingModel and call the createRating method
+                $ratingModel = new RatingModel();
+                $ratingModel->createRating($postData['username'], $postData['artist'], $postData['song'], $postData['rating']);
+                $responseData = json_encode(array('message' => 'Rating created successfully'));
+            } catch (Exception $e) {
+                $strErrorDesc = $e->getMessage();
+                $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+            }
+        } else {
+            $strErrorDesc = 'Method not supported';
+            $strErrorHeader = 'HTTP/1.1 422 Unprocessable Entity';
+        }
+
+        if (!$strErrorDesc) {
+            $this->sendOutput(
+                $responseData,
+                array('Content-Type: application/json', 'HTTP/1.1 201 Created')
+            );
+        } else {
+            $this->sendOutput(json_encode(array('error' => $strErrorDesc)),
+                array('Content-Type: application/json', $strErrorHeader)
+            );
+        }
     }
 
-    public function updateRating($username, $song, $rating)
+    public function updateAction()
     {
-        // Need to implement the logic to update an existing rating
-        $sql = "UPDATE ratings_table SET rating = ? WHERE username = ? AND song = ?";
-        $this->update($sql, ["sss", $rating, $username, $song]);
+        $strErrorDesc = '';
+        $requestMethod = $_SERVER["REQUEST_METHOD"];
+        if (strtoupper($requestMethod) == 'PUT') {
+            $postData = json_decode(file_get_contents('php://input'), true);
+
+            try {
+                // You will need to implement the logic to update a rating here
+                // Instantiate a RatingModel and call the updateRating method
+                $ratingModel = new RatingModel();
+                $ratingModel->updateRating($postData['username'], $postData['song'], $postData['rating']);
+                $responseData = json_encode(array('message' => 'Rating updated successfully'));
+            } catch (Exception $e) {
+                $strErrorDesc = $e->getMessage();
+                $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+            }
+        } else {
+            $strErrorDesc = 'Method not supported';
+            $strErrorHeader = 'HTTP/1.1 422 Unprocessable Entity';
+        }
+
+        if (!$strErrorDesc) {
+            $this->sendOutput(
+                $responseData,
+                array('Content-Type: application/json', 'HTTP/1.1 200 OK')
+            );
+        } else {
+            $this->sendOutput(json_encode(array('error' => $strErrorDesc)),
+                array('Content-Type: application/json', $strErrorHeader)
+            );
+        }
     }
 
-    public function deleteRating($username, $song)
+    public function deleteAction()
     {
-        // Need to implement the logic to delete a rating
-        $sql = "DELETE FROM ratings_table WHERE username = ? AND song = ?";
-        $this->delete($sql, ["ss", $username, $song]);
+        $strErrorDesc = '';
+        $requestMethod = $_SERVER["REQUEST_METHOD"];
+        if (strtoupper($requestMethod) == 'DELETE') {
+            $postData = json_decode(file_get_contents('php://input'), true);
+
+            try {
+                // You will need to implement the logic to delete a rating here
+                // Instantiate a RatingModel and call the deleteRating method
+                $ratingModel = new RatingModel();
+                $ratingModel->deleteRating($postData['username'], $postData['song']);
+                $responseData = json_encode(array('message' => 'Rating deleted successfully'));
+            } catch (Exception $e) {
+                $strErrorDesc = $e->getMessage();
+                $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+            }
+        } else {
+            $strErrorDesc = 'Method not supported';
+            $strErrorHeader = 'HTTP/1.1 422 Unprocessable Entity';
+        }
+
+        if (!$strErrorDesc) {
+            $this->sendOutput(
+                $responseData,
+                array('Content-Type: application/json', 'HTTP/1.1 204 No Content')
+            );
+        } else {
+            $this->sendOutput(json_encode(array('error' => $strErrorDesc)),
+                array('Content-Type: application/json', $strErrorHeader)
+            );
+        }
     }
+
+    // Add more functions as necessary
 }
 ?>
