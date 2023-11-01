6065f666dc8d946bfa2496907afe89bb9cbe377d
Jmac041 <jmcavoy@wesleyan.edu>
2023-11-01 15:44:08 -0400

    Updated backend functionality

--- backend/inc/config.php
+++ backend/inc/config.php
@@ -1,4 +1,10 @@
 <?php
+
+header("Access-Control-Allow-Origin:*");
+header("Access-Control-Allow-Headers:*");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Credentials: true");
+
 define("DB_HOST", "localhost");
 define("DB_USERNAME", "root");
 define("DB_PASSWORD", "");
