a5d2004e37804707f9fd66662ff00deaf1a51f56
Jmac041 <jmcavoy@wesleyan.edu>
2023-11-01 15:38:22 -0400

    Revert "Update index.php"
    This reverts commit 2b6017d79a80dad7f1cbdb5e99950ed8ce4eb16d.

--- backend/index.php
+++ backend/index.php
@@ -1,16 +1,6 @@
 <?php
-    header("Access-Control-Allow-Origin:*");
-    header("Access-Control-Allow-Headers:*");
-    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-    header("Access-Control-Allow-Credentials: true");
-
-        // Handle preflight requests (OPTIONS)
-    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-        http_response_code(204); // 204 No Content for preflight requests
-        exit;
-    }
-
     session_start();
+
     include "config.php";
 
     //Entry point for REST API
@@ -19,6 +9,7 @@
     // CORS Headers
     header('Access-Control-Allow-Origin:*');
     $uri = explode( '/', $uri );
+
     // UserController Actions
     if ($uri[2] == 'user') {
         require "Controller/Api/UserController.php";
@@ -34,5 +25,5 @@
         $strMethodName = $uri[3] . 'Action';
         $objRatingController->{$strMethodName}();
     }
-
+    
 ?>
